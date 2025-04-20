f00f973d2a573c54af49d063cd6b945dd5fc12d9
fartb <fartbreather228@gmail.com>
2025-04-20 19:03:55 +0300

    2 задание: добавлены комментарии

--- Program.cs
+++ Program.cs
@@ -29,8 +29,11 @@ void print_square(char symb, int side)
 
 // ZADANIE 2
 
+// т.к. это не особо важно, принимаю у пользователя
+// число строкой для удосбтва в будущем
 string user_numb = ReadLine();
 
+// проверка результата работы метода и вывод
 if (if_poly(user_numb))
 {
     WriteLine("Переданное число является паллиндромом.");
@@ -42,12 +45,18 @@ else
 
 bool if_poly(string number)
 {
+    // первая половина строки и вторая для будущего сравнения
+    // (если в строке нечётное количество символов, 
+    // средний не учитывается и не записывается ни в 1 из них)
     string f_half = number.Substring(0, (number.Length) / 2);
     string s_half = number.Substring(((number.Length - 1) / 2) + 1, (number.Length) / 2);
 
-    bool check = false;
+    // счётчик идентичности символов в строках
     int counter = 0;
 
+    // т.к. у меня почему-то не работал метод reverse, я сравниваю
+    // строки циклом, с помощью двух счётчиков, 1 из которых 
+    // идёт с отметки конца второй строки, а другой с начала первой
     for (int i = 0, j = f_half.Length - 1; i < f_half.Length || j > -1; i++, j--)
     {
         if (f_half[i] == s_half[j])
@@ -56,10 +65,13 @@ bool if_poly(string number)
         }
     }
 
+    // если счётчик срабтал при каждом символе
+    // строк, то есть они равны, возвращается тру
     if (counter == f_half.Length)
     {
         return true;
     }
+    // если счётчик меньше, то фолс
     else
     {
         return false;
