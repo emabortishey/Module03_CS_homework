cf0def77d3f97ee36a6eb9a707deca9a70d507cb
fartb <fartbreather228@gmail.com>
2025-04-20 17:16:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+// ZADANIE 1
+
+/*
+
+Напишите метод, который отображает квадрат из
+некоторого символа. Метод принимает в
+качестве параметра: длину стороны квадрата, символ.
+
+*/
+
+void print_square(char symb, int side)
+{
+    for(int i = 0; i< side; i++)
+    {
+        for(int j = 0; j < side; j++)
+        {
+            Write(symb + " ");
+        }
+        WriteLine();
+    }
+}
